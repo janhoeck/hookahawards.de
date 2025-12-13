f4376ee83fbf54876ae0fc87e92025143e038cc6
Jan Höck <jan.hoeck@deepl.com>
2025-12-13 21:05:05 +0100

    Revalidate dashboard every 10 minutes

--- apps/web/src/app/admin/dashboard/page.tsx
+++ apps/web/src/app/admin/dashboard/page.tsx
@@ -1,5 +1,7 @@
 import { DashboardView } from '@/components/views/dashboard/DashboardView'
 
+export const revalidate = 600 // every 10 minutes
+
 export default function DashboardPage() {
   return <DashboardView />
 }
