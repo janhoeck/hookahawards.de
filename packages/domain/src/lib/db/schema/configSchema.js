8aea6acfd86d4676d05d1d7772a4b7855995e07b
Jan Höck <jan.hoeck@deepl.com>
2025-12-16 14:27:43 +0100

    remove unused js files

--- packages/domain/src/lib/db/schema/configSchema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.configSchema = void 0;
-var pg_core_1 = require("drizzle-orm/pg-core");
-exports.configSchema = (0, pg_core_1.pgTable)('config', {
-    key: (0, pg_core_1.text)('key').primaryKey(),
-    value: (0, pg_core_1.jsonb)('value').$type().notNull(),
-    updatedAt: (0, pg_core_1.timestamp)('updated_at', { withTimezone: true }).notNull().defaultNow(),
-});
