6507dd658a85e8fc165444622b16b255714c12f2
Jan <jan.hoeck@gmx.net>
2025-12-18 20:18:17 +0100

    Add tanstack query

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Footer } from '@/components/shared/Footer/Footer'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Analytics } from '@vercel/analytics/next'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import React from 'react'
@@ -11,6 +12,8 @@ type LayoutProps = {
   children: React.ReactNode
 }
 
+const queryClient = new QueryClient()
+
 export default function Layout(props: LayoutProps) {
   const { children } = props
 
@@ -18,7 +21,9 @@ export default function Layout(props: LayoutProps) {
     <html className='dark'>
       <body className={twMerge(inter.className, bebasNeue.variable)}>
         <div className='flex min-h-screen flex-col'>
-          <main className='container mx-auto mt-16 mb-16 flex-1 px-4'>{children}</main>
+          <QueryClientProvider client={queryClient}>
+            <main className='container mx-auto mt-16 mb-16 flex-1 px-4'>{children}</main>
+          </QueryClientProvider>
           <Footer />
         </div>
         <Analytics />
--- apps/web/src/components/views/home/sections/categories/CategoriesSection.tsx
+++ apps/web/src/components/views/home/sections/categories/CategoriesSection.tsx
@@ -1,20 +1,34 @@
 'use client'
 
-import { useDataContext } from '@/components/contexts/data/DataContext'
 import { VotesContextProvider } from '@/components/contexts/votes/VotesContextProvider'
 import { VotingFeedback } from '@/components/views/home/VotingFeedback'
+import { fetchCategories } from '@/lib/api/categories'
+import { useQuery } from '@tanstack/react-query'
 import { Fragment } from 'react'
 
 import { ClipCategory } from './clip/ClipCategory'
 import { SurveyCategory } from './survey/SurveyCategory'
 
 export const CategoriesSection = () => {
-  const { categories } = useDataContext()
+  const { isPending, data, error } = useQuery({
+    queryKey: ['categories'],
+    queryFn: async () => {
+      return fetchCategories({ page: 1, limit: 50 })
+    },
+  })
+
+  if (isPending) {
+    return null
+  }
+
+  if (error) {
+    return null
+  }
 
   return (
     <section className='flex flex-col space-y-40'>
       <VotesContextProvider>
-        {categories.map((category) => (
+        {data.items.map((category) => (
           <Fragment key={category.id}>
             {category.type === 'clip' && <ClipCategory category={category} />}
             {category.type === 'survey' && <SurveyCategory category={category} />}
